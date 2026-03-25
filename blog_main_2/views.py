3ed68f4a4e65f7e13905bff3cc70e2fc0f06fa60
zoe0823 <zoe1104skz@gmail.com>
2026-03-24 17:01:13 -0700

    codigo views

---
+++ blog_main_2/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, render
+ 
+from .models import post, category
+
+# Create your views here.
+def home(request):
+    posts = post.objects.filter(status=post.ACTIVE).order_by('-created_at')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html',context)
