1f7c6c01228b156292e9394792e395e75a46ca47
zoe0823 <zoe1104skz@gmail.com>
2026-03-24 17:12:16 -0700

    codigo urls

--- blog_main_2/urls.py
+++ blog_main_2/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home, detail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('blog/<int:id>/', detail, name='post_detail'),
 ]
