e57bd94de6f8b40bcd8024e6e0d9d2661824da40
zoe0823 <zoe1104skz@gmail.com>
2026-03-26 16:02:03 -0700

    Codigo apps

--- blog_main_2/admin.py
+++ blog_main_2/admin.py
@@ -9,18 +9,20 @@ class ComentItemInline(admin.TabularInline):
       model = Comment
       raw_id_fields = ['post']
 
+
 class PostAdmin(admin.ModelAdmin):
       search_fields = ['title','intrp','body']
       list_display = ('title','category','created_at','status')
       list_filter = ['category','created_at','status']
       inlines = [ComentItemInline]
 
+
 class CategoryAdmin(admin.ModelAdmin):
       search_fields = ['title']
  
-list_display = ('title',)
+     list_display = ('title',)
 class CommentAdmin(admin.ModelAdmin):
-    search_fields = ('name','post','created_ad',)
+      search_fields = ('name','post','created_ad',)
 
 
 admin.site.register(post,PostAdmin)
