6bf6da07cb2b1f6784bed20303a5c62e9dc4bb26
zoe0823 <zoe1104skz@gmail.com>
2026-03-26 17:03:36 -0700

    codigo

--- blog_main_2/admin.py
+++ blog_main_2/admin.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
-
 from django.contrib import admin
 from .models import post, category, Comment
-
 
 class ComentItemInline(admin.TabularInline):
       model = Comment
       raw_id_fields = ['post']
-
 
 class PostAdmin(admin.ModelAdmin):
       search_fields = ['title','intrp','body']
@@ -16,14 +13,12 @@ class PostAdmin(admin.ModelAdmin):
       list_filter = ['category','created_at','status']
       inlines = [ComentItemInline]
 
-
 class CategoryAdmin(admin.ModelAdmin):
       search_fields = ['title']
- 
-     list_display = ('title',)
+      list_display = ('title',)
+
 class CommentAdmin(admin.ModelAdmin):
       search_fields = ('name','post','created_ad',)
-
 
 admin.site.register(post,PostAdmin)
 admin.site.register(category,CategoryAdmin)
